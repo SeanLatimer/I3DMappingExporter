30e243997ae2b129c25cd88be248b62c637342db
Sean Latimer <sean@latimer.rocks>
2018-11-27 21:18:57 +0000

    Initial Commit

---
+++ I3DMapper/Mapper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace I3DMapper
+{
+    public class Mapper
+    {
+        public XmlDocument Mappings { get; private set; }
+
+        private XmlDocument xmlDocument;
+
+        public Mapper(XmlDocument xmlDocument)
+        {
+            this.xmlDocument = xmlDocument;
+        }
+
+        public void Map()
+        {
+            Mappings = new XmlDocument();
+            var mappingsNode = Mappings.CreateElement("i3dMappings");
+
+            XmlNode scene = xmlDocument.SelectSingleNode("/i3D/Scene");
+
+
+            for (int i = 0; i < scene.ChildNodes.Count; i++)
+            {
+                XmlNode node = scene.ChildNodes[i];
+                XmlElement mapping = Mappings.CreateElement("i3dMapping");
+                mapping.SetAttribute("id", node.Attributes["name"].Value);
+                mapping.SetAttribute("node", i.ToString());
+                mappingsNode.AppendChild(mapping);
+
+                if (node.HasChildNodes)
+                {
+                    mapChildren(node.ChildNodes, i.ToString() + ">", ref mappingsNode);
+                }
+            }
+
+            Mappings.AppendChild(mappingsNode);
+        }
+
+        private void mapChildren(XmlNodeList nl, string indexPath, ref XmlElement mappingsNode)
+        {
+            for (int i = 0; i < nl.Count; i++)
+            {
+                XmlNode node = nl[i];
+                string currentPath = indexPath.EndsWith(">") ? indexPath + i.ToString() : indexPath + "|" + i.ToString();
+
+                XmlElement mapping = Mappings.CreateElement("i3dMapping");
+                mapping.SetAttribute("id", node.Attributes["name"].Value);
+                mapping.SetAttribute("node", currentPath);
+                mappingsNode.AppendChild(mapping);
+
+                if (node.HasChildNodes)
+                {
+                    mapChildren(node.ChildNodes, currentPath, ref mappingsNode);
+                }
+            }
+        }
+    }
+}
---
+++ I3DMapperUI/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace I3DMapperUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtFilePath.Text))
+            {
+                MessageBox.Show("Must select a file before exporting");
+                return;
+            }
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(txtFilePath.Text);
+
+                Task<XmlDocument> task = ExportMappings(doc);
+                btnExport.Enabled = false;
+                txtXml.Text = "Please wait, exporting mappings.";
+
+                task.Wait();
+                var mappings = task.Result;
+                btnExport.Enabled = true;
+                txtXml.Text = XDocument.Parse(mappings.OuterXml).ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        async Task<XmlDocument> ExportMappings(XmlDocument doc)
+        {
+            var mapper = new I3DMapper.Mapper(doc);
+            mapper.Map();
+            return mapper.Mappings;
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtXml.Text) || txtXml.Text == "Please wait, exporting mappings.")
+            {
+                MessageBox.Show("Please export mappings first");
+                return;
+            }
+            try
+            {
+                Clipboard.SetText(txtXml.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
