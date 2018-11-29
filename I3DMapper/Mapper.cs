3e4de3992b3fa72ea52baec285ee949f7ea003b7
Sean Latimer <sean@latimer.rocks>
2018-11-29 17:41:37 +0000

    code cleanup

--- I3DMapper/Mapper.cs
+++ I3DMapper/Mapper.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
 using System.Xml.Linq;
 
 namespace I3DMapper
