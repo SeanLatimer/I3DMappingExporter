fd3225389644c0bc0f9c26d27d4f01d634cdad68
Sean Latimer <sean@latimer.rocks>
2018-12-12 22:01:24 +0000

    Switched to Squirrel for installation & updates

---
+++ I3DMapperUI/MainForm.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace I3DMapperUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.txtXml = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.menuStripFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnPreReleaseUpdates = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnCheckForUpdates = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblUpdateStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pbUpdateProg = new System.Windows.Forms.ToolStripProgressBar();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "I3D|*.i3d";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(360, 35);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(12, 37);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(342, 20);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(12, 64);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(203, 23);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "Export Mappings";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // txtXml
+            // 
+            this.txtXml.Location = new System.Drawing.Point(12, 93);
+            this.txtXml.Multiline = true;
+            this.txtXml.Name = "txtXml";
+            this.txtXml.ReadOnly = true;
+            this.txtXml.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtXml.Size = new System.Drawing.Size(423, 349);
+            this.txtXml.TabIndex = 3;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(240, 64);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(195, 23);
+            this.btnCopy.TabIndex = 4;
+            this.btnCopy.Text = "Copy to Clipboard";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStripFile,
+            this.menuStripHelp});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(449, 24);
+            this.menuStrip.TabIndex = 5;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // menuStripFile
+            // 
+            this.menuStripFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem});
+            this.menuStripFile.Name = "menuStripFile";
+            this.menuStripFile.Size = new System.Drawing.Size(37, 20);
+            this.menuStripFile.Text = "File";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // menuStripHelp
+            // 
+            this.menuStripHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnPreReleaseUpdates,
+            this.btnCheckForUpdates});
+            this.menuStripHelp.Name = "menuStripHelp";
+            this.menuStripHelp.Size = new System.Drawing.Size(44, 20);
+            this.menuStripHelp.Text = "Help";
+            // 
+            // btnPreReleaseUpdates
+            // 
+            this.btnPreReleaseUpdates.Enabled = false;
+            this.btnPreReleaseUpdates.Name = "btnPreReleaseUpdates";
+            this.btnPreReleaseUpdates.Size = new System.Drawing.Size(180, 22);
+            this.btnPreReleaseUpdates.Text = "PreReleaseUpdates";
+            this.btnPreReleaseUpdates.Visible = false;
+            // 
+            // btnCheckForUpdates
+            // 
+            this.btnCheckForUpdates.Name = "btnCheckForUpdates";
+            this.btnCheckForUpdates.Size = new System.Drawing.Size(180, 22);
+            this.btnCheckForUpdates.Text = "Check for updates";
+            this.btnCheckForUpdates.Click += new System.EventHandler(this.btnCheckForUpdates_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblUpdateStatus,
+            this.pbUpdateProg});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 452);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(449, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip";
+            // 
+            // lblUpdateStatus
+            // 
+            this.lblUpdateStatus.Name = "lblUpdateStatus";
+            this.lblUpdateStatus.Size = new System.Drawing.Size(434, 17);
+            this.lblUpdateStatus.Spring = true;
+            this.lblUpdateStatus.Text = "Up to date";
+            this.lblUpdateStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pbUpdateProg
+            // 
+            this.pbUpdateProg.Name = "pbUpdateProg";
+            this.pbUpdateProg.Size = new System.Drawing.Size(100, 16);
+            this.pbUpdateProg.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 474);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.txtXml);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.Text = "I3D Mapping Exporter";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.TextBox txtXml;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem menuStripFile;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuStripHelp;
+        private System.Windows.Forms.ToolStripMenuItem btnCheckForUpdates;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblUpdateStatus;
+        private System.Windows.Forms.ToolStripProgressBar pbUpdateProg;
+        private System.Windows.Forms.ToolStripMenuItem btnPreReleaseUpdates;
+    }
+}
+
--- I3DMapperUI/Properties/Settings.Designer.cs
+++ I3DMapperUI/Properties/Settings.Designer.cs
@@ -8,22 +8,30 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace I3DMapperUI.Properties
-{
-
-
+namespace I3DMapperUI.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool updatePrerelease {
+            get {
+                return ((bool)(this["updatePrerelease"]));
+            }
+            set {
+                this["updatePrerelease"] = value;
             }
         }
     }
